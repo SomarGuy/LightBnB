db599289f02b71771e84c619b7df3ab0223d4c3c
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-02-17 18:02:19 -0500

    cleaned up code

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,7 +1,5 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -12,8 +10,6 @@ const pool = new Pool({
 });
 
 /// Users
-
-
 
 /**
  * Get a single user from the database given their email.
